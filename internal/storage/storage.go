6c91f997d244129d9e93d623980ebef6bc9ba0ab
Vladimir Los <vladimir.los.tech@gmail.com>
2025-01-13 21:46:06 +0500

    feat: add getting questions from file

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,9 +1,101 @@
 package storage
 
 import (
+	"bufio"
 	"github.com/vlostech/qz/internal/model"
+	"io"
+	"os"
+)
+
+const (
+	questionPart = 1
+	answerPart   = 2
 )
 
 func GetQuizSession(filePath string) (model.QuizSession, error) {
-	panic("not implemented")
+	file, err := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
+
+	if err != nil {
+		return model.QuizSession{}, err
+	}
+
+	defer func() {
+		_ = file.Close()
+	}()
+
+	items, err := extractQuizItems(file)
+
+	if err != nil {
+		return model.QuizSession{}, err
+	}
+
+	session := model.QuizSession{
+		Items: items,
+	}
+
+	return session, nil
+}
+
+func extractQuizItems(r io.Reader) ([]model.QuizSessionItem, error) {
+	curPart := questionPart
+
+	scanner := bufio.NewScanner(r)
+
+	var quizItems []model.QuizSessionItem
+	var curQuizItem *model.QuizSessionItem
+
+	idx := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		switch curPart {
+		case questionPart:
+			{
+				if text == "" {
+					if curQuizItem == nil {
+						continue
+					}
+
+					curPart = answerPart
+				} else {
+					if curQuizItem == nil {
+						curQuizItem = &model.QuizSessionItem{
+							Index:    idx,
+							Question: text,
+						}
+					} else {
+						curQuizItem.Question += "\n" + text
+					}
+				}
+			}
+		case answerPart:
+			{
+				if curQuizItem == nil {
+					panic("quizItem was not initialized")
+				}
+
+				if text == "" {
+					if curQuizItem.ExpectedAnswer == "" {
+						continue
+					} else {
+						quizItems = append(quizItems, *curQuizItem)
+						curQuizItem = nil
+						idx++
+						curPart = questionPart
+					}
+				} else {
+					if curQuizItem.ExpectedAnswer == "" {
+						curQuizItem.ExpectedAnswer = text
+					} else {
+						curQuizItem.ExpectedAnswer += "\n" + text
+					}
+				}
+			}
+		}
+	}
+
+	quizItems = append(quizItems, *curQuizItem)
+
+	return quizItems, nil
 }
