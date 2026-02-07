6c0c7586d0e9330deebf00e8d6e0f0cb241c1fc5
Vladimir Los <vladimir.los.tech@gmail.com>
2026-02-07 20:45:34 +0500

    test: add tests for ranges

---
+++ internal/ranges/ranges_test.go
@@ -0,0 +1,109 @@
+package ranges
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_ParseRange(t *testing.T) {
+	tests := []struct {
+		name     string
+		rangeStr string
+		want     RangeQuery
+		wantErr  bool
+	}{
+		{
+			name:     "ReturnsFullRangeWhenEmptyStringProvided",
+			rangeStr: "",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 0, CloseIndex: -1}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsSingleRangeWhenSingleIndexProvided",
+			rangeStr: "42",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 42, CloseIndex: 43}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsSingleRangeWhenConsecutiveNumbersProvided",
+			rangeStr: "5, 6, 7",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 5, CloseIndex: 8}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsTwoRangesWhenGapBetweenNumbers",
+			rangeStr: "5, 6, 8",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{
+					{OpenIndex: 5, CloseIndex: 7},
+					{OpenIndex: 8, CloseIndex: 9},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsFullRangeWhenTwoDotsProvided",
+			rangeStr: "..",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 0, CloseIndex: -1}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsRangeWhenHalfOpenRangeFromIndexToEnd",
+			rangeStr: "42..",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 42, CloseIndex: -1}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsRangeWhenHalfOpenRangeFromBeginningToIndex",
+			rangeStr: "..42",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 0, CloseIndex: 43}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsRangeWhenSimpleRangeProvided",
+			rangeStr: "3..6",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{{OpenIndex: 3, CloseIndex: 7}},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "ReturnsMultipleRangesWhenComplexStringProvided",
+			rangeStr: "..10, 15, 20, 30..40, 50..",
+			want: RangeQuery{
+				Parts: []RangeQueryPart{
+					{OpenIndex: 0, CloseIndex: 11},
+					{OpenIndex: 15, CloseIndex: 16},
+					{OpenIndex: 20, CloseIndex: 21},
+					{OpenIndex: 30, CloseIndex: 41},
+					{OpenIndex: 50, CloseIndex: -1},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseRange(tt.rangeStr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseRange() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseRange() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
